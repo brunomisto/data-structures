4e56a82a65e62ab7b71d5d01e6f31b85790a7644
misto <ottonellicopattibruno@gmail.com>
2024-01-21 12:49:19 -0300

    Use solid principle for reusability

--- LinkedList.js
+++ LinkedList.js
@@ -1,165 +1,164 @@
 class Node {
-    constructor(value = null, next = null) {
-        this.value = value;
-        this.next = next;
-    }
+  constructor(value = null, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 
-    toString() {
-        return `( ${this.value} ) -> `;
-    }
+  toString() {
+    return `( ${this.value} ) -> `;
+  }
 }
 
 export default class LinkedList {
-    constructor() {
-        this.head = null;
+  constructor() {
+    this.head = null;
+  }
+
+  append(node) {
+    if (!this.head) {
+      this.head = node;
+      return;
     }
 
-    append(value) {
-        if (!this.head) {
-            this.head = new Node(value);
-            return;
-        }
+    let pointer = this.head;
+    while (pointer.next) {
+      pointer = pointer.next;
+    }
+    pointer.next = node;
+  }
 
-        let pointer = this.head;
-        while(pointer.next) {
-            pointer = pointer.next;
-        }
-        pointer.next = new Node(value);
+  prepend(node) {
+    if (!this.head) {
+      this.head = node;
+      return;
     }
 
-    prepend(value) {
-        if (!this.head) {
-            this.head = new Node(value);
-            return;
-        }
-        
-        let newNode = new Node(value);
-        newNode.next = this.head;
-        this.head = newNode;
+    let newNode = node;
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  get size() {
+    let count = 0;
+    let pointer = this.head;
+    while (pointer) {
+      count++;
+      pointer = pointer.next;
+    }
+    return count;
+  }
+
+  get tail() {
+    if (!this.head) {
+      return null;
     }
 
-    get size() {
-        let count = 0;
-        let pointer = this.head;
-        while(pointer) {
-            count++;
-            pointer = pointer.next;
-        }
-        return count;
+    let pointer = this.head;
+    while (pointer.next) {
+      pointer = pointer.next;
     }
+    return pointer;
+  }
 
-    get tail() {
-        if (!this.head) {
-            return null;
-        }
-
-        let pointer = this.head;
-        while(pointer.next) {
-            pointer = pointer.next;
-        }
+  at(index) {
+    let count = 0;
+    let pointer = this.head;
+    while (pointer) {
+      if (count === index) {
         return pointer;
+      }
+      pointer = pointer.next;
+      count++;
+    }
+    return null;
+  }
+
+  pop() {
+    if (!this.head) {
+      return;
     }
 
-    at(index) {
-        let count = 0;
-        let pointer = this.head;
-        while(pointer) {
-            if (count === index) {
-                return pointer;
-            }
-            pointer = pointer.next;
-            count++;
-        }
-        return null;
+    if (!this.head.next) {
+      this.head = null;
+      return;
     }
 
-    pop() {
-        if (!this.head) {
-            return;
-        }
-
-        if (!this.head.next) {
-            this.head = null;
-            return;
-        }
-
-        let pointer = this.head;
-        while(pointer.next.next) {
-            pointer = pointer.next;
-        }
-
-        pointer.next = null;
+    let pointer = this.head;
+    while (pointer.next.next) {
+      pointer = pointer.next;
     }
 
-    contains(value) {
-        let pointer = this.head;
-        while(pointer) {
-            if (pointer.value === value) {
-                return true;
-            }
-            pointer = pointer.next;
-        }
-        return false;
+    pointer.next = null;
+  }
+
+  contains(value) {
+    let pointer = this.head;
+    while (pointer) {
+      if (pointer.value === value) {
+        return true;
+      }
+      pointer = pointer.next;
+    }
+    return false;
+  }
+
+  find(value) {
+    let pointer = this.head;
+    let count = 0;
+    while (pointer) {
+      if (pointer.value === value) {
+        return count;
+      }
+      pointer = pointer.next;
+      count++;
+    }
+    return null;
+  }
+
+  toString() {
+    let string = "";
+    let pointer = this.head;
+    while (pointer) {
+      string += pointer.toString();
+      pointer = pointer.next;
+      if (!pointer) {
+        string += "null";
+      }
+    }
+    return string;
+  }
+
+  insertAt(index, node) {
+    if (index === 0) {
+      this.prepend(node);
+      return;
     }
 
-    find(value) {
-        let pointer = this.head;
-        let count = 0;
-        while(pointer) {
-            if (pointer.value === value) {
-                return count;
-            }
-            pointer = pointer.next;
-            count++;
-        }
-        return null;
+    let pointer = this.head;
+    let count = 0;
+
+    while (pointer) {
+      if (pointer.next && count + 1 === index) {
+        let newNode = node;
+        newNode.next = pointer.next;
+        pointer.next = newNode;
+      }
+      pointer = pointer.next;
+      count++;
+    }
+  }
+
+  removeAt(index) {
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
     }
 
-    toString() {
-        let string = '';
-        let pointer = this.head;
-        while(pointer) {
-            string += pointer.toString();
-            pointer = pointer.next;
-            if (!pointer) {
-                string += "null";
-            }
-        }
-        return string;
+    let pointer = this.head;
+    let count = 0;
+
+    if (pointer.next && count + 1 === index) {
+      pointer.next = pointer.next.next;
     }
-
-    insertAt(index, value) {
-        if (index === 0) {
-            this.prepend(value);
-            return;
-        }
-
-        let pointer = this.head;
-        let count = 0;
-
-
-        while(pointer) {
-            if (pointer.next && count + 1 === index) {
-                let newNode = new Node(value);
-                newNode.next = pointer.next;
-                pointer.next = newNode;
-            }
-            pointer = pointer.next;
-            count++;
-        }
-    }
-
-    removeAt(index) {
-        if (index === 0) {
-            this.head = this.head.next;
-            return;
-        }
-
-        let pointer = this.head;
-        let count = 0;
-
-        if (pointer.next && count + 1 === index) {
-            pointer.next = pointer.next.next;
-        }
-    }
+  }
 }
